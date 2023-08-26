32e369a80dfcbe167dc54e782929ee74fced6306
unknown <anands2807@gmail.com>
2023-08-26 09:56:48 +0530

    svg images and data created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {useContext} from "react";
 import { AppContext, useGlobalContext } from "./context";
+import hero from './images/hero.svg';
+import logo from './images/logo.svg';
+import phone from './images/phone.svg';
 
 function App() {
 
   return (
     <div >
       <h1>Modern website layout</h1>
+      
     </div>
   );
 }
