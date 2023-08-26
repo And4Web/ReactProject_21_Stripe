1f17f0a513a1b933c909a35545b330f2d5176fa9
unknown <anands2807@gmail.com>
2023-08-26 11:46:41 +0530

    context.js finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {useContext} from "react";
 import { AppContext, useGlobalContext } from "./context";
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Hero from './Hero';
+import Submenu from './Submenu';
+
 import hero from './images/hero.svg';
 import logo from './images/logo.svg';
 import phone from './images/phone.svg';
 
 function App() {
-
+  
   return (
     <div >
-      <h1>Modern website layout</h1>
+      <Navbar/>
+      <Sidebar/>
+      <Hero/>
+      <Submenu/>
       
     </div>
   );
--- src/context.js
+++ src/context.js
@@ -1,12 +1,23 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
+import sublinks from './data';
 
 export const AppContext = React.createContext();
 
 export const AppContextProvider = ({children}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  
+  const openSidebar = () => {setIsSidebarOpen(true)};
+  const closeSidebar = () => {setIsSidebarOpen(false)};
+
+  const openSubmenu = () => {setIsSubmenuOpen(true)};
+  const closeSubmenu = () => {setIsSubmenuOpen(false)};
+
+  
+
+
   return <AppContext.Provider value={{
-    firstEl: "this is first element",
-    secondEl: "this is second element",
-    thirdEl: "this is third element"
+    isSubmenuOpen, isSidebarOpen, openSubmenu, openSidebar, closeSubmenu, closeSidebar
   }}>{children}</AppContext.Provider>
 }
 
