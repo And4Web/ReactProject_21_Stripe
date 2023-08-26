7c5079608d4f4302017e05a2d62bda5e970bf635
unknown <anands2807@gmail.com>
2023-08-26 12:47:09 +0530

    Hero component created

--- src/Hero.js
+++ src/Hero.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import phoneImg from './images/phone.svg';
 
 function Hero() {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infractructure for the internet</h1>
+          <p>Millions of companies of all sizes from startups to fortune 500s use Stripe's software and APIs to accept payments, send payouts, and manage their businesses online.</p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
   )
 }
 
